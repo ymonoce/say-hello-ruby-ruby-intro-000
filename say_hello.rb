881e0c892bd43e48ac52e2c8e4ad7b70a378b8c9
Chloe Atwood <atwoodksw@gmail.com>
2020-10-16 23:34:48 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,6 @@
-# Build your say_hello method here
+# defines method with default parameter
+def say_hello(name=Ruby Progammer)
+  # display "Hello, insert argument value, !"
+  puts Hello (name) !
+  # end method definition
+end
